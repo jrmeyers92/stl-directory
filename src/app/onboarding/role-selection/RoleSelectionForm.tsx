e9ca345baf005f4502d5eb4903217aab68576328
Jake Meyers <jakermeyers92@gmail.com>
2025-06-27 16:14:54 -0500

    fix eslint errors

--- src/app/onboarding/role-selection/RoleSelectionForm.tsx
+++ src/app/onboarding/role-selection/RoleSelectionForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { getUserRole } from "@/utils/roles";
 import { useSession } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
